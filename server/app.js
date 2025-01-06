5dd19e78c3e034e10a5b8f95ea2e39e68af72afc
Angel <dev.aguevara@synergycodelabs.com>
2025-01-06 01:39:11 -0500

    fix: enable CORS headers in production environment

--- server/app.js
+++ server/app.js
@@ -14,24 +14,22 @@
   // Initialize Express app
   const app = express();
 
-  // Development CORS settings
-  if (environment.isDevelopment()) {
-    app.use((req, res, next) => {
-      const origin = req.headers.origin;
-      
-      if (environment.ALLOWED_ORIGINS.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.header('Access-Control-Allow-Credentials', 'true');
-      }
-      
-      if (req.method === 'OPTIONS') {
-        return res.sendStatus(204);
-      }
-      next();
-    });
-  }
+  // CORS middleware for all environments
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    
+    if (environment.ALLOWED_ORIGINS.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', 'true');
+    }
+    
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+    next();
+  });
   
   // Load embeddings on startup
   loadEmbeddings(__dirname);
