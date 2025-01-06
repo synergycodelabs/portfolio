9d4f29b546173091f98e19ae2c801365efb0e18e
Angel <dev.aguevara@synergycodelabs.com>
2025-01-06 02:02:54 -0500

    fix: remove duplicate CORS middleware from server.js

--- server/server.js
+++ server/server.js
@@ -6,24 +6,6 @@ import app from './app.js';
 import environment from './config/environment.js';
 import logger from './utils/logger.js';
 
-// CORS settings for all environments
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  
-  if (environment.ALLOWED_ORIGINS.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
-
 // Create HTTP server
 const httpServer = http.createServer(app);
 
